08fd3013eff3cccb0fed50e2f0ee6d20d268782a
lianghui <1170464456@qq.com>
2022-07-07 19:13:45 +0800

    初始化

---
+++ src/main/java/com/example/wei_day1/WeiDay1Application.java
@@ -0,0 +1,13 @@
+package com.example.wei_day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiDay1Application.class, args);
+    }
+
+}
